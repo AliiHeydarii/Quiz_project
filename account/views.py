9ae36dfe277551c520d16d233fbd0447a32f9c63
Ali Heydari <heydari1375.ali@gmail.com>
2025-03-02 19:46:31 +0330

    create account urls and base html template

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register_user(request):
+    pass
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
