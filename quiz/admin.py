6562ecd6fb11be84f6a8edfe47494ea10f693c83
Ali Heydari <heydari1375.ali@gmail.com>
2025-03-03 11:27:18 +0330

    create quiz models and register in admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Question,Quiz,Choice,UserAnswer
 
-# Register your models here.
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(Choice)
+admin.site.register(UserAnswer)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    
+class Question(models.Model):
+    text = models.TextField()
+    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE,related_name='questions')
+
+    def __str__(self):
+        return self.text
+
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='choices')
+    text = models.CharField(max_length=300)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+    
+    
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='answers')
+    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='user_answers')
+    selected_choice = models.ForeignKey(Choice,on_delete=models.CASCADE,related_name='user_selections')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_correct(self):
+        return self.selected_choice.is_correct
+
+    def __str__(self):
+        return f'{self.user.email}-{self.question.text}-{self.selected_choice.text}'
