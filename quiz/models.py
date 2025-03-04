e1eab6f160769039338f6b2b47f80b97016c9972
Ali Heydari <heydari1375.ali@gmail.com>
2025-03-04 12:00:26 +0330

    add logic for quiz detail view for save user score

--- quiz/models.py
+++ quiz/models.py
@@ -46,8 +46,9 @@ class Question(models.Model):
 
 class UserAnswer(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='answers')
-    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='user_answers')
+    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.email}-{self.question.text}-{self.selected_choice.text}'
+        return f'{self.user.email}-{self.quiz.title}-{self.score}'
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Quiz,Question
+from .models import Quiz,Question,UserAnswer
 
 def index_view(request):
     quiz_list = Quiz.objects.all()
@@ -9,13 +9,27 @@ def index_view(request):
 def quize_detail(request,pk):
     quiz = Quiz.objects.get(pk=pk)
     questions = Question.objects.filter(quiz=quiz)
+
+    if UserAnswer.objects.filter(user = request.user , quiz = quiz).exists():
+        return render(request,'quiz/already_taken.html')
+
     if request.method == 'POST':
         score = 0
         for question in questions:
             answer = request.POST.get(f'question_{question.id}')
             if answer == question.correct_answer :
                 score += 1
-        print(score)
+
+        user_answer = UserAnswer.objects.filter(user=request.user).first()
+        if user_answer:
+            print('user')
+        else:
+            UserAnswer.objects.create(
+                user = request.user,
+                quiz = quiz , 
+                score = score
+            )
+        return render(request , 'quiz/result.html' , {'score' : score})
     return render(request,'quiz/quiz_detail.html' , {'questions' : questions})
 
 
